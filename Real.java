969de2f1481d2008746d582786e3b023d3e43459
bgsahan <bgsahan@yahoo.com>
2017-02-03 16:56:46 +0200

    Update Real.java

--- Real.java
+++ Real.java
@@ -1,1 +1,36 @@
-Public class Varrt{}
+public class Singleton{
+ 
+  private static Singleton mInstance = null;
+  private String mExample;
+  
+  private Singleton(){
+    }
+  
+  public static Singleton getInstance(){
+    if(mInstance == null){
+      mInstance = new Singleton();
+      }
+    return mInstance;
+    }
+  
+  public String getExample(){
+    return this.mExample;
+    }
+  
+  public String setExample(String value){
+    mExample = value;
+    }
+  
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
