9c498f13cfd46ae1c08039b88a0d48eb74010bee
bgsahan <bgsahan@yahoo.com>
2017-02-09 14:45:50 +0200

    Update Real.java

--- Real.java
+++ Real.java
@@ -22,7 +22,7 @@ public class Singleton{
     }
  
  
- //WRITE FILE. Problem: Context: Activity? get.Activity?
+ //WRITE FILE. Problem: Context: Activity? get.Activity(): context?
  private final String FILENAME = "testfile.txt";
  ArrayList<Project> projectArrayList;
   
@@ -30,10 +30,11 @@ public class Singleton{
   try{
     FileOutputStream fileOutputStream = getActivity().openFileOutput(FILENAME, Context.PRIVATE_CONTEXT);
     ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-    objectOutputStream.writeInt(projectArrayList.size());
-    for(Projekt p:projektArrayList)
-      objectOutStream.writeObject(r);
+    objectOutputStream.writeInt(arrayList.size());
+    for(Projekt p:arrayList)
+      objectOutStream.writeObject(p);
     objectOutStream.close();
+    fileOutputStream.close();
    }
   catch(java.io.IOException e){
     Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -45,7 +46,8 @@ public class Singleton{
 public ArrayList<Project> readFile(){
   FileInputStream fileInputStream = openFileInput(FILENAME);
   ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-  projectArrayList = (ArrayList<Projekt> objectInputStream.readObject();                  
+  projectArrayList = (ArrayList<Projekt> objectInputStream.readObject();  
+  return projectArrayList;
   
   
   
