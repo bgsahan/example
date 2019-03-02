8474b8b6cbc7f2bceb086e4d7731b25bd98d7fea
bgsahan <bgsahan@yahoo.com>
2019-03-03 00:45:47 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,21 @@
+public class Test extends Activity {
+  
+  TextView mTextView;
+  
+  protected void onCreate(Bundle savedInstanceState) {
+    
+    super.onCreate(savedInstanceState);
+    setContentview(R.layout.main_activity_layout);
+    
+    mTextview = (TextView) findViewById(R.id.text_view);
+    
+  }
+  
+  public void sayHello (View view) {
+    
+    mTextView.setText("Hello");
+    
+  }
+  
+}
 
