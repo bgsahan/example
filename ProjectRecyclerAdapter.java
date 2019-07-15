cfcd634aef12f257e2b5e54cbad0afb5b672e99b
bgsahan <bgsahan@yahoo.com>
2019-07-15 20:22:02 +0300

    Create ProjectRecyclerAdapter.java

---
+++ ProjectRecyclerAdapter.java
@@ -0,0 +1,96 @@
+public class ProjectRecyclerAdapter extends RecyclerView.Adapter<ProjectRecyclerAdapter.ProjectRecyclerViewHolder> {
+
+   private final LinkedList<Project> mProjectList;
+   private final LayoutInflater mInflater;
+
+
+   class ProjectRecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+
+       public final TextView projectItemView;
+
+       final ProjectRecyclerAdapter mAdapter;
+
+
+       public ProjectRecyclerViewHolder(View itemView, ProjectRecyclerAdapter adapter) {
+
+           super(itemView);
+
+           projectItemView = (TextView) itemView.findViewById(R.id.project_textView);
+
+           this.mAdapter = adapter;
+
+           itemView.setOnClickListener(this);
+
+       }
+
+
+         //Directing from MainActivity to the ProjectActivity
+
+         @Override
+
+         public void onClick(View v) {
+
+           Intent intent = new Intent(getActivity(), ProjectActivity.class);
+
+           intent.putExtra("project_id", getAdapterPosition());
+
+           startActivity(intent);
+
+         }
+
+       }
+
+
+
+   }
+
+
+   public ProjectRecyclerAdapter(Context context, LinkedList<Project> projectList) {
+
+       mInflater = LayoutInflater.from(context);
+
+       this.mProjectList = projectList;
+
+   }
+
+
+   @Override
+
+   public ProjectRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+       // Inflate an item view.
+
+       View mItemView = mInflater.inflate(R.layout.projrctlist_item, parent, false);
+
+       return new ProjectRecycleriewHolder(mItemView, this);
+
+   }
+
+
+   @Override
+
+   public void onBindViewHolder(ProjectRecyclerViewHolder viewHolder, int position) {
+
+       // Retrieve the data for that position.
+
+       String mCurrentProjectTitle = mProjectList.get(position).getProjectTitle();
+
+       // Add the data to the view holder.
+
+       viewHolder.projectItemView.setText(mCurrentProjectTitle);
+
+
+   }
+
+
+   @Override
+
+   public int getItemCount() {
+
+       return mProjectList.size();
+
+   }
+
+}
+
