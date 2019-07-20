33ee990c17fee359664a79555d48b24cbbb66546
bgsahan <bgsahan@yahoo.com>
2019-07-20 11:45:10 +0300

    Adding Singleton and revising adapter / database

--- MainActivity.java
+++ MainActivity.java
@@ -3,20 +3,25 @@ public class MainActivity extends Activity {
  //RecyclerView declarations
  private RecyclerView mRecyclerView;
  private ProjectRecyclerAdapter mProjectRecyclerAdapter;
- private final LinkedList<String> mProjectList = new LinkedList<>();
+ //private final ArrayList<String> mProjectList = new ArrayList<Project>();
 
  //Room Database declarations
  ProjectDatabase mProjectDatabase;
-
-
+  
+ //Create Singleton
+ Singleton singleton = Singleton.getInstance();
+  
  private void onCreate(Bundle savedInstanceState) {
    super.onCreate(savedInstanceState);
    setContentView(R.layout.main_activity_layout);
-
+  
+   //Custom method for loading all Room Database projects using LiveData
+   loadAllProjects();
+   
    mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
 
    // Create an RecyclerView adapter and supply the data to be displayed.
-   mAdapter = new ProjectRecyclerAdapter(this, mProjectList);
+   mAdapter = new ProjectRecyclerAdapter(this, singleton.getProjectList());
 
    //Room Database initialising & building
    mProjectDatabase = Room.databaseBuilder(getApplicationContext(), ProjectDatabase.class, "PROJECT_DATABASE").build();
@@ -27,10 +32,7 @@ public class MainActivity extends Activity {
    // Give the recycler view a default layout manager.
    mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-   //Custom method for loading all Room Database projects using LiveData
-   loadAllProjects(); 
-
-   //TODO:Add FAB for projecg adding
+   //TODO:Add FAB for project add
 
    //TODO: Add Toolbar and Options Menu 
 
@@ -41,7 +43,8 @@ public class MainActivity extends Activity {
    mProjectDatabase.daoAcces().loadAllProjects().observe(this, new Observer<List<Project>>() {
      @Override
      public void onChanged(@Nullable List<Project> projectList) {
-       mAdapter.mProjectList = projectList;
+       singleton.setProjectList(projectList);
+       mAdapter.mProjectList = singleton.getProjectList();
        mAdapter.notifyDatasetChanged();
      }
 
