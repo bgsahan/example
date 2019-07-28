147c6c87768e7d622342ddde3d34b94b05e3ec78
bgsahan <bgsahan@yahoo.com>
2019-07-28 14:52:45 +0300

    Create ProjectActivity.java

---
+++ ProjectActivity.java
@@ -0,0 +1,18 @@
+public class ProjectActivity extends Activity {
+
+  int projectId;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstaneState)
+    setContentView(R.layout.project_activity_layout);
+    
+    Intent incomingIntent = getIntent();
+    projectId = incomingIntent.getIntExtra(MainActivity.EXTRA_PROJECT_ID);
+    
+    //TODO: Check related project id from Room Database and bring related data. Singleton should be used?
+    
+    //TODO: Implement RecyclerView
+    
+  }
+}
